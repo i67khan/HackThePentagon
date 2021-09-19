d9369cebb9756392df2c562ce9890b16e9306929
Ibrahim Khan <i67khan@uwaterloo.ca>
2021-09-19 08:37:43 -0400

    Final

--- study-buddy/src/pages/Settings.js
+++ study-buddy/src/pages/Settings.js
@@ -1,9 +1,15 @@
 import '../styles/Settings.css';
-
+import Stock from "../Stock-guy.png";
 function Settings() {
     return (
         <div className="App">
             <header className="App-header">
+                <img src={Stock} className="Profile-Pic"></img>
+                <div className="Upload-doc">
+                    <body className="Up-label">Upload a Profile Picture</body>
+                    <body className="Me">Update about me</body>
+                    <a href="/" className="Submit">Sign Out</a>
+                </div>
             </header>
         </div>
     );
