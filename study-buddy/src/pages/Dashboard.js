62086a80ad5cafeaa8c6d84d391f5678246374be
Ibrahim Khan <i67khan@uwaterloo.ca>
2021-09-19 07:27:58 -0400

    Almost done

--- study-buddy/src/pages/Dashboard.js
+++ study-buddy/src/pages/Dashboard.js
@@ -1,12 +1,26 @@
+import { Component } from 'react';
 import '../styles/Dashboard.css';
 
-function Dashboard() {
-    return (
-        <div className="App">
-            <header className="App-header">
-            </header>
-        </div>
-    );
+class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <body className="Welcome-dash">Welcom Ibrahim,</body>
+                    <body className="You-dash">You are taking</body>
+                    <div className="Course">
+                        <body className="CS245">CS245</body>
+                    </div>
+                    <div className="Add">
+                        <body className="Plus">+</body>
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default Dashboard;
--- study-buddy/src/pages/Login.js
+++ study-buddy/src/pages/Login.js
@@ -1,12 +1,45 @@
 import '../styles/Login.css';
+import {Component} from 'react';
 
-function Login() {
-    return (
-        <div className="App">
-            <header className="App-header">
-            </header>
-        </div>
-    );
+class Login  extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "E: Email",
+            password: "P: Password"
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            email: event.target.email,
+            password: event.target.password
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div className="Login-box">
+                        <body className="Welcome">Welcome Back</body>
+                        <body className="Question"> Ready for another study session?</body>
+                        <form action='/dashboard'>
+                            <div className="f1">
+                                <input type='text' className="Email" value={this.state.email} onChange={this.handleChange} />
+                            </div>
+                            <div className="f2">
+                                <input type="text" className="Password" value={this.state.password} onChange={this.handleChange} />
+                            </div>
+                            <button className="Log-in-button">Log In</button>
+                        </form>
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default Login;
